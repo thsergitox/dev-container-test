b035e745f112358a901855223350522cef48937e
thsergitox <sergiopezoj@gmail.com>
2024-10-21 14:55:54 -0500

    Add Dockerfile and devcontainer.json for development environment setup

---
+++ Actividades/pruebas_fixtures/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Modelo de datos
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
